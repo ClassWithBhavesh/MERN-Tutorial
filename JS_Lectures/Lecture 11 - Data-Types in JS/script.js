dfe28f0c9cfb0191bedd517ac997fd6abb60169a
Bhavesh Joshi <classwithbhavesh@gmail.com>
2025-10-03 18:08:53 +0530

    Add files via upload

---
+++ JS_Lectures/Lecture 11 - Data-Types in JS/script.js
@@ -0,0 +1,65 @@
+// Data-Types in JS - the type of data stored in a variable will be knnown as Data-Types
+
+// Data-Types in JS -
+// 1. Primitive Data-Type - number | string | boolean | undefined | null
+// 2. Reference Data-Type (non-primitive) - Arrays  | Objects
+
+// primitive data-types
+// String -
+// let num_var = "Anmol";
+// console.log(num_var);
+// console.log(typeof(num_var));
+
+// number -
+// let num_var = 25;
+// console.log(num_var);
+// console.log(typeof(num_var));
+
+// boolean -
+// let condition = false;
+// console.log(condition)
+// console.log(typeof(condition))
+
+// undefined -
+// let unDefinedVariable;
+// console.log(unDefinedVariable);
+// console.log(typeof(unDefinedVariable));
+
+// null -
+// let nullVariable = null;
+// console.log(nullVariable);
+// console.log(typeof(nullVariable));
+
+// reference data-type -
+
+// 1. array - in a well defined collection of similar or different type of data stored in a variable,  enclosed in [] braces and each&every element is separated by comma (,)
+// What is index in array - it is a unique identity provided to each and every element of the array
+// value of index in an array varies from 0 to array(length - 1)
+
+// let arr = [10,20,30,60];
+// console.log(arr);
+// console.log(arr[0]);
+// console.log(arr[1]);
+// console.log(arr[2]);
+// console.log(arr[3]);
+// console.log(typeof(arr));
+
+// 2. object - is a well defined collection of similar or different type of data stored in a variable but in the form of key-value pair, enclosed in {} braces and just like array, each and every element is separated by a comma(,)
+
+// let obj = {
+//     key: "value"
+// }
+
+// Array - 
+// let arr = [10,20,30,60];
+// console.log(arr);
+
+// Object - 
+// const empData = {
+//   empName: "Anurag",
+//   empAge: 35,
+//   empSalary: 35000,
+//   empName: "Kunal"
+// };
+
+// console.log(empData);
