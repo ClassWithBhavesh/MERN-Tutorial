fcd5c1d283869ccdb8a99d81422e5b5d9f3694b9
Bhavesh Joshi <classwithbhavesh@gmail.com>
2025-10-04 12:38:29 +0530

    operators in JS covered

---
+++ JS_Lectures/Lecture 12 - Operators in JS/script.js
@@ -0,0 +1,116 @@
+// Operators in JS
+// 1. Arithematic operator
+// 2. Assignment Operator
+// 3. Comparison Operator
+// 4. Logical Operator
+// 5. Ternary Operator
+
+// Operation - is a task or a procedure for a specific program which uses some operators to perform operation
+// Operands  - are the values or terms on which we will apply operations
+// Operators - are symbols to perform operation between two or more operands
+
+
+// 1. Arithmatic Operators -->  +, -, *. /, %, **
+
+// let num1 = 25;
+// let num2 = 16;
+// console.log(num1 + num2)
+// console.log(num1 - num2)
+// console.log(num1 * num2)
+// console.log(num1 / num2)
+// console.log(num1 % num2)
+// console.log(num1 ** num2)
+// document.write(num1+num1)
+
+// console.log((num2 + 5 ) + (- num2));
+
+// console.log((num2 + 5 ) * (- num2));
+
+// console.log((num2 / 5 ) * (- num1));
+
+// console.log(num1 % num3)
+
+
+// let num1 = 5;
+// let num2 = 4;
+
+// console.log(num1**num2)
+
+
+
+
+
+
+
+// 2. Assignment Operators -->  =,  +=, -=, *=, /=, **=, %=
+
+// let noOfAams = 12;
+// let oneDozen = noOfAams;
+
+// console.log(noOfAams)
+// console.log(oneDozen)
+
+
+// let num1 = 52;
+// let num2 = 25;
+
+// console.log(num1)
+// console.log(num2)
+
+// num1 += num2            // num1 = num1 + num2
+// console.log(num1)
+// console.log(num2)
+
+// num1 -= num2            // num1 = num1 - num2
+// console.log(num1)
+// console.log(num2)
+
+// num1 *= num2            // num1 = num1 * num2
+// console.log(num1)
+// console.log(num2)
+
+// num1 /= num2            // num1 = num1 / num2
+// console.log(num1)
+// console.log(num2)
+
+// num1 **= num2            // num1 = num1 ** num2
+// console.log(num1)
+// console.log(num2)
+
+// num1 %= num2            // num1 = num1 % num2
+// console.log(num1)
+// console.log(num2)
+
+
+
+
+
+
+// 3. Comparison Operators - ==, ===, !=, >, <, >=, <=
+
+// let num1 = 52.000001;
+// let num2 = 52;
+// let num2 = 25;
+// console.log(num1)
+// console.log(num2)
+// console.log(num1 == num2)
+// console.log(num1 === num2)
+// console.log(num1 != num2)
+// console.log(num1 > num2)
+// console.log(num1 < num2)
+// console.log(num1 <= num2)
+// console.log(num1 >= num2)
+
+
+
+// 4. Logical Operator --> && - and / || - or / ! - not
+
+// let num1 = 52.000001;
+// let num2 = 52;
+// let num3 = 25;
+
+// console.log(num1 > num2 && num1 == num3);
+// console.log(num1 > num2 || num1 == num3);
+
+// console.log(!(num1 > num2 && num1 == num3));
+// console.log(!(num1 > num2 || num1 == num3));
