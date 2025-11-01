82d76056b9fb8e1b8af66c10c288795e5fc8bf6a
Bhavesh Joshi <classwithbhavesh@gmail.com>
2025-11-01 13:17:44 +0530

    String methods in JavaScript

---
+++ JS_Lectures/Lecture 18 - String Methods/String_Methods.js
@@ -0,0 +1,118 @@
+
+
+// // Strings :
+
+// // 1. Length :
+// var str = "This is a JavaScript Tutorial.";
+// var l = str.length;
+// document.write(l + "<br><br>");
+
+// // 2. toLowerCase :
+// var str = "Let's See the SECOND String Method";
+// var l = str.toLowerCase();
+// document.write(l + "<br><br>");
+
+// // 3. toUpperCase :
+// var str = "let's see the third string method";
+// var u = str.toUpperCase();
+// document.write(u + "<br>");
+
+// // 4. includes :
+// var str = "let's see the fourth string method";
+// var i = str.includes("let's see");
+// document.write(i + "<br>");
+
+// // 5. startsWith :
+// var str = "let's see the fifth string method";
+// var i = str.startsWith("fifth");
+// document.write(i + "<br>"); 
+
+// // 6. endsWith :
+// var str = "let's see the sixth string method";
+// var i = str.endsWith("method");
+// document.write(i + "<br>");
+
+// // 7. search :
+// var str = "let's see the sixth string method";
+// var i = str.search("s");
+// document.write(i + "<br>");
+
+// // 8. match :
+// var str = "let's see the eigth string method";
+// var i = str.match(/t/g);
+// document.write(i + "<br>");
+
+// // 9. indexOf :
+// var str = "let's see the ninth string method";
+// var i = str.indexOf("s");
+// document.write(i + "<br>");
+
+// // 10. lastIndexOf :
+// var str = "let's see the ninth string method";
+// var i = str.lastIndexOf("h");
+// document.write(i + "<br>");
+
+// // 11. replace :
+// var str = "let's see the ninth see string see method see";
+// var i = str.replace(/see/g, "move"); // (// can be used for more then one replace)
+// document.write(i + "<br>");
+
+
+// // 12. trim :
+// var str = "      Hello       Students";
+// document.write(str);
+// console.log(str);
+// document.write(str);
+// var a = str.trim();
+// console.log(a);
+
+// // 13. charAt :
+// var str = "let's see the ninth string method";
+// var i = str.charAt(32); // (// can be used for more then one replace)
+// document.write(i + "<br>");
+
+// // 14. charCodeAt :
+// var str = "let's see the ninth string method";
+// var i = str.charCodeAt(32);
+// document.write(i + "<br>");
+
+// var i = String.fromCharCode(65);
+// document.write(i + "<br>"); 
+
+// 15. concat  :
+// var str = "I'm a student";
+// var s = " and i belong to Indore";
+// var a = str.concat(s);
+// document.write(a + "<br>");
+
+// // 16. split :
+// var str = "JavaScript is a intresting language";
+// var a = str.split("i");
+// document.write(a + "<br>");
+
+// // 17. repeat :
+// var str = "JavaScript is a intresting language";
+// var a = str.repeat(3);
+// document.write(a + "<br>");
+
+// // 18. slice :
+// var str = "JavaScript is a intresting language";
+// // var a = str.slice(10);
+// var a = str.slice(10);
+// document.write(a + "<br>");
+
+// // 19. substr :
+// var str = "JavaScript is a intresting language";
+// var a = str.substr(3,6);
+// document.write(a + "<br>");
+
+// // 20. substring :
+// var str = "JavaScript is a intresting language";
+// var a = str.substring(3,6);
+// document.write(a + "<br>");
+
+// // 21. toString :
+// var st = 25014;
+// var str = st.toString();
+// document.write(str + "<br>");
+// document.write(typeof(str));
