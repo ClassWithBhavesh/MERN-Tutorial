94d4deb1d1deedff98d3d0d2ad2712c6a6c589f6
Bhavesh Joshi <classwithbhavesh@gmail.com>
2025-11-03 17:39:30 +0530

    Event Handling covered, heading towards the DOM

---
+++ JS_Lectures/Lecture 19 - Event Handling/dom.js
@@ -0,0 +1,4 @@
+// DOM - stands for Document Object Model, it is a tree like structure in which each and every node or branches denotes a specific element or parent element in that docuement (HTML doc)
+
+// console.log(document);
+
---
+++ JS_Lectures/Lecture 19 - Event Handling/event.js
@@ -0,0 +1,50 @@
+// Events in JavaScript - is a certain action through which we can perform certain operations or a specific tasks
+
+
+// const msgFunc = () => {
+//     console.log("This is Box 1");
+// }
+
+// msgFunc()
+
+
+// let divEle = document.getElementById("cont1");
+// console.log(divEle);
+
+
+// divEle.addEventListener("click", msgFunc);
+
+// divEle.addEventListener("click", (e) => {
+//     console.log("This is Div Element");
+//     console.log(e.target);
+// })
+
+
+// divEle.addEventListener("dblclick", (e) => {
+//     console.log("This is Div Element");
+//     console.log(e.target);
+// })
+
+
+// divEle.addEventListener("contextmenu", (e) => {
+//     console.log("This is Div Element");
+//     console.log(e.target);
+// })
+
+
+// divEle.addEventListener("onscroll", (e) => {
+//     console.log("This is Div Element");
+//     console.log(e.target);
+// })
+
+
+
+// let htmlBody = document;
+// console.log(htmlBody)
+
+// htmlBody.addEventListener("scroll", msgFunc);
+
+
+
+
+// htmlBody.addEventListener("load", msgFunc);
