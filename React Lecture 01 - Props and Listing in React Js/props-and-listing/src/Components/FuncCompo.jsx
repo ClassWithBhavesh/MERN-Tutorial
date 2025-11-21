5f369507656fab3a300ebc8dbbeb9cf1fd28c591
Bhavesh Joshi <classwithbhavesh@gmail.com>
2025-11-21 17:35:32 +0530

    Covered with Props and Listing in React

---
+++ React Lecture 01 - Props and Listing in React Js/props-and-listing/src/Components/FuncCompo.jsx
@@ -0,0 +1,21 @@
+
+// function FuncCompo() {
+//   return (
+//     <div>FuncCompo</div>
+//   )
+// }
+
+// export default FuncCompo
+
+
+// export default - only one module can be exported
+// Named export   - can export as much components as we wants to export
+
+
+// const FuncCompo = () => {
+//     return (
+//         <h1>I am Func Compo</h1>
+//     )
+// }
+
+// export default FuncCompo;
